edb5fd6ded25f5c77e2cebbcef39611587910a97
ojchevyx <ojchevyx@gmail.com>
2024-04-05 15:22:56 -0400

    Migrating main.cpp from test robot branch to main

--- Proteus_Project/main.cpp
+++ Proteus_Project/main.cpp
@@ -4,87 +4,99 @@
 int main(void)
 {
 
-    const float SPEED = 50;
+    /**
+     * menuLabels = labels for icon array on Proteus starting screen
+     * leftIGWAN = shared pointer for left igwan motor
+     * rightIGWAN = shared pointer for right igwan motor
+     * cdsSensor = shared pointer for cds sensor
+     * leftEncoder = shared pointer for left encoder
+     * rightEncoder = shared pointer for right encoder
+     * armServo = shared pointer for servo located on left side of robot
+     * frontServo = shared pointer for servo located on front of robot
+     * selection = int value that controls the switch statement based on icon array input
+    */
 
-    char menuLabels[4][20] = { "System Chk", "Run", "Calibrate", "Quit" };
-    //bump switches
-    DigitalInputPin frontSwitch(FEHIO::P0_3);
-    //cds sensor
-    AnalogInputPin cdsSensor(FEHIO::P1_0);
-    //IGWAN motor shaft encoders
-    DigitalEncoder leftEncoder(FEHIO::P0_7);
-    DigitalEncoder rightEncoder(FEHIO::P0_0);
+    char menuLabels[4][20] = { "System Chk", "Run", "Calibrate", "Bty Life" };
 
+    auto leftIGWAN = std::make_shared<FEHMotor>(FEHMotor::Motor0, 9.0);
+    auto rightIGWAN = std::make_shared<FEHMotor>(FEHMotor::Motor1, 9.0);
 
-    //IGWAN Motors
-    FEHMotor leftIGWAN(FEHMotor::Motor0, 9.0);
-    FEHMotor rightIGWAN(FEHMotor::Motor1, 9.0);
+    auto cdsSensor = std::make_shared<AnalogInputPin>(FEHIO::P1_0);
 
-    // //Servo Motors
-    // FEHServo plateServo(FEHServo::Servo0);
-     FEHServo armServo(FEHServo::Servo0);
+    auto leftEncoder = std::make_shared<DigitalEncoder>(FEHIO::P0_7);
+    auto rightEncoder = std::make_shared<DigitalEncoder>(FEHIO::P0_0);
 
-     armServo.SetMin(1290);
-     armServo.SetMax(2085);
-
-    float xPos;
-    float yPos;
-
-    Controller controller;
-    Robot robot;
+    auto armServo = std::make_shared<FEHServo>(FEHServo::Servo0);
+    auto frontServo = std::make_shared<FEHServo>(FEHServo::Servo7);
 
     int selection = -2;
 
+    // armServo->SetMin(1290);
+    // armServo->SetMax(2215);
+
+    //Setting the min and max values for the arm servo
+    armServo->SetMin(500);
+    armServo->SetMax(2000);
+
+    //Setting the min and max values for the front servo
+    frontServo->SetMin(500);
+    frontServo->SetMax(2320);
+
+    //Creating an object of robot class using custom constructor
+    Robot robot = Robot(leftIGWAN, rightIGWAN, cdsSensor, leftEncoder, rightEncoder, armServo, frontServo);
+
+    //While loop for icon array, returns back to icon array after each function completes
     while (selection != -1) {
 
-        selection = controller.GUIControl(menuLabels);
+        //Getting value of selection from robot.GUIControl function, pass in menuLabels to
+        // label icon array
+        selection = robot.GUIControl(menuLabels);
 
+        //Switch statement based on gui control selection
         switch (selection) {
 
             case 0:
 
-                //TODO: Call System check
-                //Iteration for System Check: 10
-                //robot.SystemCheck(leftIGWAN, rightIGWAN, leftEncoder, rightEncoder);
+                //Run robot.SystemCheck() when System check is selected in icon array
+                //Iteration for System Check: 36
+                //robot.SystemCheck();
+                robot.ServoTest();
                 //controller.MoveStraight(leftIGWAN, rightIGWAN, -40, rightEncoder, 20, 1.75);
-                controller.DisplayCDSSensorValue(cdsSensor);
+                //controller.DisplayCDSSensorValue(cdsSensor);
                 break;
 
             case 1:
 
-                //TODO: Call run function for course
-                //robot.MoveTowardRamp(leftIGWAN, rightIGWAN, SPEED);
-                //Iterations for First Progress Check: 75
-                //Iterations for Second Progress Check: 78
-                //Iterations for Third Progress Check: 23
-                robot.RunProgressCheck3(leftIGWAN, rightIGWAN, leftEncoder, rightEncoder, cdsSensor, armServo);
+                //Run robot.Run() when Run is selected in icon array
+                //Iterations for Third Progress Check: 24
+                //Iterations for Fourth Progress Check: 28
+                //Iterations for Fifth Progress Check: 40
+                //Owen is a bitch
+                robot.RunProgressCheck5();
                 break;
 
             case 2:
 
-                //TODO: Calibrate various motors
-                //Iteration: 3
-                robot.Calibrate(armServo);
+                //Call a calibrate function so servos can be easily calibrated
+                //Iteration: 5
+                robot.Calibrate();
+                break;
+
+            case 3:
+
+                //Call a battery life function to check battery life of Proteus when it has been selected from menu
+                //Iteration: 16
+                robot.BatteryLife();
                 break;
 
             default:
 
-                //TODO: Quit out of program, tell user that proteus controller has been quit
+                //Quit out of the icon array if something goes wrong
                 break;
 
         }
 
     }
-
-    // controller.SetMotor(leftIGWAN, 15);
-    // controller.SetMotor(rightIGWAN, -15);
-
-    // rightEncoder.ResetCounts();
-
-    // while (rightEncoder.Counts() < 300);
-
-    // controller.SetMotor(leftIGWAN, 0);
-    // controller.SetMotor(rightIGWAN, 0);
 
 	return 0;
 }
